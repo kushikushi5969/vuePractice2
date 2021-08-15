641bc357dd0e1d7e46f822990a1bc39c0668f2b1
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-08-15 16:38:34 +0900

    jsアニメーション

--- vue-practice2/src/App.vue
+++ vue-practice2/src/App.vue
@@ -6,6 +6,7 @@
     <button @click="show = !show">切り替え</button>
     <br><br>
     <transition
+      :css="false"
       @before-enter="beforeEnter"
       @enter="enter"
       @after-enter="afterEnter"
@@ -70,26 +71,45 @@ export default {
   methods: {
     beforeEnter(el) {
       // 現れる前
+      el.style.transform = `scale(0)`
     },
     enter(el, done) {
       // 現れる時
+      let scale = 0;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1
+        if (scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 100);
     },
-    afterEnter(el) {
+    afterEnter() {
       // 現れたあと
     },
-    enterCancelled(el) {
+    enterCancelled() {
       // 現れるアニメーションがキャンセルされた時
     },
-    beforeLeave(el) {
+    beforeLeave() {
       // 消える前
     },
     leave(el, done) {
       // 消える時
+      let scale = 1;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1
+        if (scale < 0) {
+          clearInterval(interval);
+          done();
+        }
+      }, 100);
     },
-    afterLeave(el) {
+    afterLeave() {
       // 消えた後
     },
-    leaveCancelled(el) {
+    leaveCancelled() {
       // 消えるアニメーションがキャンセルされた時
     },
   }
