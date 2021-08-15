8b7060e360de5b4653aa380b8851ed4d66fcf7bf
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-08-15 15:46:59 +0900

    複数の要素を切り替える

--- vue-practice2/src/App.vue
+++ vue-practice2/src/App.vue
@@ -4,6 +4,16 @@
     <button @click="myAnimation = 'fade'">Fade</button>
     <p>{{ myAnimation}}</p>
     <button @click="show = !show">切り替え</button>
+    <transition name="fade">
+      <p
+        v-if="show"
+        key="bye"
+      >さよなら</p>
+      <p
+        v-else
+        key="hello"
+      >こんにちは</p>
+    </transition>
     <transition
       enter-active-class="animate__animated animate__bounce"
       leave-active-class="animate__animated animate__wobble"
